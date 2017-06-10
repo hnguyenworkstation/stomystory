9e4e127d52b56c65b14a2ceafea184bc582093a8
Hung Nguyen <hnguyenworkstation@gmail.com>
2017-06-10 08:49:56 -0400

    fixed references

--- src/App.js
+++ src/App.js
@@ -2,24 +2,23 @@
  * Created by hungnguyen on 6/9/17.
  */
 
-import {React,} from 'react';
+import React from 'react';
 import MainActivity from './activities/main_activity'
-
-import {Scene, Router} from 'react-native-router-flux';
+import {Scene, Router, Actions} from 'react-native-router-flux';
 
 
 const scenes = Actions.create(
-  <Scene key="root">
-    <Screen key="main_activity" component={MainActivity} title="Hoocons"/>
+  <Scene key='root'>
+    <Screen key='home' component={MainActivity} title='StomyStory'/>
   </Scene>
 );
 
 class App extends React.Component {
     render() {
         return (
-            <Router>
-                return <Router scenes={scenes}/>
-            </Router>
-        )
+            <Router scenes={scenes}/>
+        );
     }
 }
+
+export default App;
--- src/activities/main_activity.js
+++ src/activities/main_activity.js
@@ -3,25 +3,41 @@
  */
 'use strict';
 
-const React = require('react-native', 'react-native-material-bottom-navigation');
-const {
-  View,
-  StyleSheet,
-  ScrollView,
-  ViewPagerAndroid,
-  Platform, BottomNavigation, Tab
-} = React;
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import {View, Text, StyleSheet} from 'react-native'
+import BottomNavigation, { Tab } from 'react-native-material-bottom-navigation'
+import React, {Component} from 'react'
 
-import {Icon,} from 'react-native-vector-icons/MaterialIcons'
-
-class MainActivity extends React.Component {
+export default class MainActivity extends React.Component {
     render() {
         return (
             <View>
-                <BottomNavigation>
-                    <Tabbar
+                <BottomNavigation
+                    labelColor="white"
+                    rippleColor="white"
+                    style={styles.bottom_tab}>
+
+                    <Tab
                         BackgroundColor="#3E2723"
-                        label="Newsstand"
+                        label="NewsStand"
+                        icon={<Icon size={24} color="white" name="newspaper"/>}
+                    />
+
+                    <Tab
+                        BackgroundColor="#3E2723"
+                        label="NewsStand"
+                        icon={<Icon size={24} color="   white" name="newspaper"/>}
+                    />
+
+                    <Tab
+                        BackgroundColor="#3E2723"
+                        label="NewsStand"
+                        icon={<Icon size={24} color="white" name="newspaper"/>}
+                    />
+
+                    <Tab
+                        BackgroundColor="#3E2723"
+                        label="NewsStand"
                         icon={<Icon size={24} color="white" name="newspaper"/>}
                     />
                 </BottomNavigation>
@@ -30,15 +46,8 @@ class MainActivity extends React.Component {
     }
 }
 
-var style = Stylesheet.create({
-    container: {
-       flex: 1
-    },
-    scrollview: {
-        flex: 1,
-        backgroundColor: 'transparent',
-    },
-    card: {
-        backgroundColor: 'transparent',
+var styles = StyleSheet.create({
+    bottom_tab: {
+        height: 56, elevation: 8, position: 'absolute', left: 0, bottom: 0, right: 0,
     }
 });
