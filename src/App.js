e3a7a55dea50ee7abb8e0e0988b521df3b9c18d7
Hung Nguyen <hnguyenworkstation@gmail.com>
2017-06-11 11:05:18 -0400

    added indicator for viewpager

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@
  */
 
 import React from 'react';
-import MainActivity from './activities/main_activity'
-import {Scene, Router, Actions} from 'react-native-router-flux';
+import {Actions, Scene, Router} from 'react-native-router-flux';
+
+
+import MainActivity from './activities/main_activity';
 
 
 const scenes = Actions.create(
   <Scene key='root'>
-    <Screen key='home' component={MainActivity} title='StomyStory'/>
+    <Screen key='home' component={MainActivity} title='MainActivity'/>
   </Scene>
 );
 
 class App extends React.Component {
-    render() {
+    static render() {
         return (
             <Router scenes={scenes}/>
         );
